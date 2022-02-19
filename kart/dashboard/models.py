049315c3d64966cf586a3ac2540283439b586621
Dhanushkumar-S-G <dhanushkumarganapathy@gmail.com>
2022-02-19 10:01:29 +0530

    made some changes

--- kart/dashboard/models.py
+++ kart/dashboard/models.py
@@ -8,6 +8,13 @@ class Book(models.Model):
     title = models.CharField(verbose_name="Title", max_length=50)
     picture = models.ImageField(verbose_name="Photo", upload_to="book")
     desc = models.TextField(verbose_name="Description")
+    author = models.TextField(verbose_name="Author", null=True)
+    pub_year = models.IntegerField(verbose_name="Year published", null=True)
+    book_edition = models.IntegerField(verbose_name="Book_edition", null=True)
+    dept = models.TextField(verbose_name="Department ", null=True)
+    sem = models.IntegerField(verbose_name="Semester", null=True)
+    date_posted = models.DateField(verbose_name="Date Posted", auto_now_add="True",null=True)
+    to_lend = models.BooleanField(verbose_name="To lend", default=True)
 
     def __str__(self):
         return self.title + " " + str(self.roll_no)
--- kart/dashboard/views.py
+++ kart/dashboard/views.py
@@ -7,8 +7,17 @@ from .models import Book
 # Create your views here.
 def dashboard(request):
     if request.session.get('roll_no'):
-        student = Student.objects.get(roll_no=request.session['roll_no'])
-        return render(request, 'dashboard/layout.html', {'student': student})
+        roll_no = request.session['roll_no']
+        student = Student.objects.get(roll_no=roll_no)
+        books = Book.objects.raw(
+            "select * from dashboard_book where roll_no_id  = '" + roll_no + "' and to_lend = True;")
+        if request.method == 'POST':
+            print("exe")
+            book_id = request.POST.get('hide')
+            book = Book.objects.get(id=book_id)
+            book.to_lend = False
+            book.save()
+        return render(request, 'dashboard/dashboard.html', {'student': student, 'books': books})
     else:
         return redirect('/')
 
@@ -39,7 +48,8 @@ def profile(request):
 def explore(request):
     if request.session.get('roll_no'):
         student = Student.objects.get(roll_no=request.session['roll_no'])
-        books = Book.objects.all()
+        books = Book.objects.raw(
+            "select * from dashboard_book where to_lend = True order by date_posted;")
         return render(request, 'dashboard/explore.html', {'student': student, 'books': books})
     else:
         redirect('/')
@@ -53,6 +63,11 @@ def lend(request):
             desc = request.POST.get('description')
             picture = request.FILES.get('picture')
             book = Book(roll_no=student, title=title, desc=desc, picture=picture)
+            book.author = request.POST.get('author')
+            book.pub_year = request.POST.get('year')
+            book.book_edition = request.POST.get('edition')
+            book.dept = request.POST.get('department')
+            book.sem = request.POST.get('semester')
             book.save()
             return redirect('/me')
 
@@ -72,6 +87,7 @@ def view_profile(request, roll=None):
 
     else:
         return redirect('/')
+
 
 def error404(request, exception):
     return render(request, 'dashboard/404.html')
